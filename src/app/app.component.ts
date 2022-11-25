41181e519da0d29b1ea6f93282b21f4d01c5f697
hiranwj <hiranwj.connect@gmail.com>
2022-11-25 10:35:11 +0530

    Initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,30 @@
 import { Component } from '@angular/core';
 
+type TodoItem = {
+  checked: boolean,
+  description: string
+};
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'to-do-app';
+  toDoItems: TodoItem[] = [];
+
+  getItems(complete: boolean = false): TodoItem[] {
+    return this.toDoItems.filter(item => item.checked === complete);
+  }
+
+  addItem(description: string): void{
+    this.toDoItems.push({
+      checked: false,
+      description: description
+    });
+  }
+
+  removeItem(item: TodoItem): void{
+    this.toDoItems.splice(this.toDoItems.indexOf(item), 1);
+  }
 }
